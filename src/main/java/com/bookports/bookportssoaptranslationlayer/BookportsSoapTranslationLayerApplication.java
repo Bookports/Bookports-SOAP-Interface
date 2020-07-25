e01d202742ea6e97c4bba6c22d79846e8c040e3e
Dom Rodriguez <shymega@shymega.org.uk>
2020-07-26 00:04:15 +0100

    Initial commit

---
+++ src/main/java/com/bookports/bookportssoaptranslationlayer/BookportsSoapTranslationLayerApplication.java
@@ -0,0 +1,13 @@
+package com.bookports.bookportssoaptranslationlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookportsSoapTranslationLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookportsSoapTranslationLayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookports/bookportssoaptranslationlayer/BookportsSoapTranslationLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookports.bookportssoaptranslationlayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookportsSoapTranslationLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
